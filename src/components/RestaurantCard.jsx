685847bbd9a3643a4776a33affde607598a28701
Kalidass <kalidass.k021@gmail.com>
2024-10-13 18:46:36 +0530

    add styles to restaurant card

--- src/components/RestaurantCard.jsx
+++ src/components/RestaurantCard.jsx
@@ -1,6 +1,7 @@
 import { CDN_URL } from '../utils/constants';
 import { motion } from 'framer-motion';
-import { IoIosStar } from 'react-icons/io';
+import { FaStar } from 'react-icons/fa';
+import { MdDeliveryDining } from 'react-icons/md';
 
 const RestaurantCard = (props) => {
   const { resData } = props;
@@ -10,28 +11,40 @@ const RestaurantCard = (props) => {
   const { deliveryTime } = sla;
   return (
     <motion.div
-    whileHover={{
-      scale: 0.98,
-      transition: {duration: 0.1}
-    }}
-
-    whileTap={{scale: 1}}
+      whileHover={{
+        scale: 0.98,
+        transition: { duration: 0.1 },
+      }}
+      whileTap={{ scale: 1 }}
     >
       <div
         data-testid='resCard'
-        className='m-4 mt-0 p-4 w-[250px] h-[370px] rounded-t-md border-b-2'
+        className='m-4 mt-0 p-4 w-[250px] h-[383px] rounded-t-md border-b-2'
       >
         <img
-          className='rounded-lg w-[220px] h-[138.5px]'
+          className='rounded-lg w-[273px] h-[182px]'
           alt='res-logo'
           src={`${CDN_URL}${cloudinaryImageId}`}
         />
         <h3 className='font-bold py-4 text-lg'>{name}</h3>
         {/* {console.log('cuisines', cuisines.join(','))} */}
-        <h4 className='text-sm text-gray-600'>{cuisines.join(',').length < 25 ? cuisines.join(' ,') : cuisines.join(' ,').slice(0, 20)}{'...'}</h4>
-        <h4 className='text-sm text-gray-600'>{costForTwo}</h4>
-        <div className='flex'><h4 className='text-sm text-gray-600'>{avgRating}</h4><span className='pl-[2px] pt-[2px]'><IoIosStar size={14} color='#165b42' /></span></div>
-        <h4 className='text-sm text-gray-600'>{deliveryTime} mins</h4>
+        <h4 className='text-sm text-gray-600'>
+          {cuisines.join(',').length < 25
+            ? cuisines.join(' ,')
+            : cuisines.join(' ,').slice(0, 20)}
+          {'...'}
+        </h4>
+        <h4 className='text-sm text-gray-600 pb-1'>{costForTwo}</h4>
+        <div className='pb-1'>
+        <div className='absolute mt-[1.5px]'>
+          <FaStar size={15} color='green'/>
+        </div>
+        <h4 className='text-sm text-gray-600 ml-[20px]'>{avgRating}</h4>
+        </div>
+        <div className='absolute pt-[1.5px]'>
+          <MdDeliveryDining size={18} color='#ff5200' />
+        </div>
+        <h4 className='text-sm text-gray-600 ml-[20px]'>{deliveryTime} mins</h4>
       </div>
     </motion.div>
   );

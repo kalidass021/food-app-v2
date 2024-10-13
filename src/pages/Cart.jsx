718c373715782e6dd66bfbfeaf3d3093f3ab1144
Kalidass <kalidass.k021@gmail.com>
2024-10-13 18:26:55 +0530

    add styles to cart page

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,3 +1,5 @@
+import { MdOutlineRemoveShoppingCart } from 'react-icons/md';
+import { BsCartXFill } from 'react-icons/bs';
 import { useDispatch, useSelector } from 'react-redux';
 import ItemList from '../components/ItemList';
 import { clearCart } from '../redux/slices/cartSlice';
@@ -12,21 +14,39 @@ const Cart = () => {
   };
   return (
     <div className='text-center m-4 p-4 min-h-screen'>
-      <h1 className='text-2xl font-bold'>Cart</h1>
+      <h1 className='text-2xl font-bold mt-[30px]'>Cart</h1>
+      <div>
+        <div
+          tabIndex={0}
+          className='card absolute ml-[850px] card-compact dropdown-content bg-base-100 z-[1] mt-3 w-52 shadow'
+        ></div>
+      </div>
       <div className='w-6/12 m-auto'>
+        <ItemList itemCards={cartItems} />
         {/* cartItems.length === 0 */}
         {!cartItems.length ? (
-          <h1>Cart is empty, Add items to the cart</h1>
+          <div className='mt-[80px] h-255px items-center'>
+            <span className='text-xl'>
+              Cart is empty, Add items to the cart
+            </span>
+            <div>
+              <span className='flex ml-[297px] mt-[30px]'>
+                <BsCartXFill size={60} color='ff5200' />
+                <span className='loading loading-ball loading-lg absolute ml-3 bg-[#ff5200]'></span>
+              </span>
+            </div>
+          </div>
         ) : (
           <button
-            className='p-2 m-2 bg-green-600 text-white rounded-lg'
+            className='m-2 btn flex bg-[#ff5200] hover:bg-[#ff3200] text-white'
             onClick={handleClearCart}
           >
-            Clear Cart
+            Clear
+            <span>
+              <MdOutlineRemoveShoppingCart size={20}/>
+            </span>
           </button>
         )}
-
-        <ItemList itemCards={cartItems} />
       </div>
     </div>
   );

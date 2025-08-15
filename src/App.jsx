2b4ac9e0daf62d987a091acbf6c008bac215845f
Kalidass <kalidass.k021@gmail.com>
2025-08-16 00:11:11 +0530

    install and config vercel analytics component to track the user interactions in the application

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { Analytics } from '@vercel/analytics/react'; // for tracking the user interactions in the application
 import appStore from './redux/appStore';
 import Header from './components/Header';
 import Footer from './components/Footer';
@@ -12,6 +13,7 @@ const App = () => {
         <Outlet />
         <Footer />
       </Provider>
+      <Analytics />
     </>
   );
 };
